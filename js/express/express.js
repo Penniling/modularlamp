9c065d3bf379a039579e57df36b9167a6e46dec9
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-01 17:39:15 +0200

    Update express.js

--- js/express/express.js
+++ js/express/express.js
@@ -1,6 +1,7 @@
 module.exports = (app, express) => {
     var bodyParser = require('body-parser')
     app.use(bodyParser.json())
+    app.use(express.json())
     app.use((req, res, next) => {
     if (/https?:\/\/192.168.(\d+)?.(\d+)?(:\d+)?\/?/.test(req.origin)) {
         next();

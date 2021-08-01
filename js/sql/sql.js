14295e579d0cf2d4c12ddace53cfe20fe2363ac3
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-01 13:32:08 +0200

    advancerd query for testing

--- js/sql/sql.js
+++ js/sql/sql.js
@@ -2,6 +2,6 @@ module.exports = (con) => {
     con.connect((err) => {
         if (err) console.log(err);
         console.log("Connected to server")
-        con.query('UPDATE `lamp`.`Users` SET `tokens`="moin" WHERE `name`="penniling" AND `password`=1234')
+        con.query(`UPDATE 'lamp'.'Users' SET 'tokens'="${Math.random}" WHERE 'name'="${"test"}" AND 'password'=${1234}`)
     })
 }

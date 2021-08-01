7c6a1fca2a6dcca27641d8a1188471f4f6b29630
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-01 17:25:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var bodyParser = require('body-parser')
 const socketio = require("socket.io");
 const express = require("express");
 const mysql = require("mysql");
@@ -5,6 +6,7 @@ const http = require("http");
 require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.json())
 const server = http.createServer(app);
 
 app.get("/", (req, res) => {

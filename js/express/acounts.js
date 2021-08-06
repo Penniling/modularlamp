25ae10ef748f3ce0ce993c7fad4bb15277791a07
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-06 20:07:02 +0200

    Update acounts.js

--- js/express/acounts.js
+++ js/express/acounts.js
@@ -1,6 +1,6 @@
 module.exports = (app, con) => {
     app.post("/register", (req, res) => {
-        if (!req.body.key == "abc") {res.status(402); res.end(); return}
+        //if (!req.body.key == "abc") {res.status(402); res.end(); return}
         con.query('INSERT INTO `Users`(`name`, `password`) VALUES("'+req.body.usr+'", "'+req.body.pwd+'")', (error, results, fields) => {
             if(error) throw error;
             res.end()

da9f709336dfec9ad54053c8a18e1fa3b028fdd6
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-06 20:18:38 +0200

    asdasdf

--- js/express/acounts.js
+++ js/express/acounts.js
@@ -1,5 +1,6 @@
 module.exports = (app, con) => {
     app.post("/register", (req, res) => {
+        console.log(req.body)
         if (req.body.key != "abc") {res.status(401); res.end(); return}
         con.query('INSERT INTO `Users`(`name`, `password`) VALUES("'+req.body.usr+'", "'+req.body.pwd+'")', (error, results, fields) => {
             if(error) throw error;

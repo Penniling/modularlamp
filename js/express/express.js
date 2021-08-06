2f040067c065cf00c0713c7ec75e5587e7d04541
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-06 20:26:15 +0200

    Update express.js

--- js/express/express.js
+++ js/express/express.js
@@ -1,11 +1,12 @@
 module.exports = (app, express) => {
     app.use(express.json())
     app.use((req, res, next) => {
-    if (/(https?:\/\/)?192.168.(\d+)?.(\d+)?(:\d+)?\/?/.test(req.origin)) {
-        next();
-    } else {
-        res.status(401);
-        res.end();
-    }
-    });
+        console.log(req.origin)
+        if (/(https?:\/\/)?192.168.(\d+)?.(\d+)?(:\d+)?\/?/.test(req.origin)) {
+            next();
+        } else {
+            res.status(401);
+            res.end();
+        }
+        });
 }

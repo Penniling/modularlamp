012c83699b74b71bb4f6ed4a022248810fb64b79
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-06 20:25:00 +0200

    Update express.js

--- js/express/express.js
+++ js/express/express.js
@@ -1,7 +1,7 @@
 module.exports = (app, express) => {
     app.use(express.json())
     app.use((req, res, next) => {
-    if (/https?:\/\/192.168.(\d+)?.(\d+)?(:\d+)?\/?/.test(req.origin)) {
+    if (/(https?:\/\/)?192.168.(\d+)?.(\d+)?(:\d+)?\/?/.test(req.origin)) {
         next();
     } else {
         res.status(401);

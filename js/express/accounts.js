a5ecce78b1acc12e31eb6bc9244d542d83c8d3d9
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-07 16:30:39 +0200

    as

--- js/express/accounts.js
+++ js/express/accounts.js
@@ -3,7 +3,7 @@ module.exports = (app, con) => {
     app.post("/register", (req, res) => {
         console.log(req.body)
         if (req.body.key != "abc") {res.status(401); res.end(); return}
-        con.query('INSERT INTO `Users`(`name`, `password`, `tokens`) VALUES(?, ?, [])', (req.body.usr, bcrypt.hashSync(unescape(encodeURIComponent([req.body.pwd])), process.env.salts)), (error, results, fields) => {
+        con.query('INSERT INTO `Users`(`name`, `password`, `tokens`) VALUES(?, ?, [])', (req.body.usr, bcrypt.hashSync(unescape(encodeURIComponent(["abc"])), process.env.salts)), (error, results, fields) => {
             if(error) throw error;
             res.end()
         })
--- js/express/authentication.js
+++ js/express/authentication.js
@@ -12,7 +12,7 @@ module.exports = (app, con) => {
     function validateCred(cred) {
         con.query('SELECT `password` FROM `Users` WHERE `name`=?', (cred.usr), (error, results, fields) => {
             if (error) return false;
-            return results[0] == bcrypt.hashSync(unescape(encodeURIComponent([cred.pwd])), process.env.salts)
+            return results[0] == bcrypt.hashSync(unescape(encodeURIComponent(["abc"])), process.env.salts)
         })
     }
 

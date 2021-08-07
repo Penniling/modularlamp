acfbc750c1f91635296031a8b315278abed7a82f
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-07 17:06:39 +0200

    Update authentication.js

--- js/express/authentication.js
+++ js/express/authentication.js
@@ -3,16 +3,23 @@ module.exports = (app, con) => {
     const bcrypt = require("bcrypt")
 
     app.post("/login", (req, res) => {
-        if (!validateCred(req.body)) {res.status(401); res.end(); return};
-        token = [...generateToken()].join("");
-        con.query('UPDATE `Users`; SET `tokens` = JSON_ARRAY_APPEND(`tokens`, "$", CAST("?" as JSON)) WHERE `name` = ?;', ({"a": "b"}, req.body.usr), (error, results, fields) => {
-            if (error) return
+        validateCred(req.body).then((valid) => {
+            if (!valid) {res.status(401); res.end(); return};
+            token = [...generateToken()].join("");
+            con.query('UPDATE `Users`; SET `tokens` = JSON_ARRAY_APPEND(`tokens`, "$", CAST("?" as JSON)) WHERE `name` = ?;', ({"a": "b"}, req.body.usr), (error, results, fields) => {
+                if (error) return
+            })
+        }).catch(() => {
+            res.status(500)
+            res.end()
         })
       });
     function validateCred(cred) {
-        con.query('SELECT `password` FROM `Users` WHERE `name`=?', (cred.usr), (error, results, fields) => {
-            if (error) return false;
-            return results[0] == bcrypt.hashSync(cred.pwd, parseInt(process.env.salts))
+        return new Promise((resolve, reject) => {
+            con.query('SELECT `password` FROM `Users` WHERE `name`=?', (cred.usr), (error, results, fields) => {
+                if (error) reject();
+                resolve(results[0] == bcrypt.hashSync(cred.pwd, parseInt(process.env.salts)))
+            })
         })
     }
 

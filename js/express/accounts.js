4e201182300977242ca4d5e015eb43bbf0de6504
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-07 17:43:12 +0200

    Update accounts.js

--- js/express/accounts.js
+++ js/express/accounts.js
@@ -4,7 +4,7 @@ module.exports = (app, con) => {
         console.log(req.body)
         if (req.body.key != "abc") {res.status(401); res.end(); return}
         const salt = bcrypt.genSaltSync(parseInt(process.env.salts))
-        con.query('INSERT INTO `Users`(`name`, `password`, `tokens`) VALUES(?, ?, "[]")', [req.body.usr, bcrypt.hashSync(unescape(encodeURIComponent("abc")), salt)], (error, results, fields) => {
+        con.query('INSERT INTO `Users`(`name`, `password`, `tokens`) VALUES(?, ?, "[]")', [req.body.usr, bcrypt.hashSync(unescape(encodeURIComponent(req.body.pwd)), salt)], (error, results, fields) => {
             if(error) throw error;
             res.end()
         })

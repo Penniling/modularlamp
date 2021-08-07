c8f00e4e17994ef1f8f1ced2b4f5d0c3e3e7892a
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-07 17:37:33 +0200

    Update authentication.js

--- js/express/authentication.js
+++ js/express/authentication.js
@@ -21,6 +21,7 @@ module.exports = (app, con) => {
                 console.log(results[0]["password"])
                 const salt = bcrypt.genSaltSync(parseInt(process.env.salts))
                 console.log(bcrypt.hashSync(unescape(encodeURIComponent("abc")), salt))
+                console.log(bcrypt.hashSync(unescape(encodeURIComponent("abc")), salt))
                 resolve(results[0]["password"] == bcrypt.hashSync(unescape(encodeURIComponent("abc")), salt))
             })
         })

5ce25a178c127dbdf1fb955bf5f063e7d43112ed
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-08 13:22:29 +0200

    Update express.js

--- js/express/express.js
+++ js/express/express.js
@@ -1,8 +1,4 @@
 module.exports = (app, express) => {
-    const cors = require("cors")
-    app.use(cors({
-        origin: "*",
-    }))
     app.use((req, res, next) => {
         console.log(req.socket.remoteAddress)
         if (/192.168.(\d+)?.(\d+)?(:\d+)?\/?/.test(req.socket.remoteAddress)) {

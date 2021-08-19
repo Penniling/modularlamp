33b767d00596c634733855bd7eb959f107a4f980
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-19 17:38:15 +0200

    Update lamp_io.js

---
+++ js/io/lamp_io.js
@@ -0,0 +1,17 @@
+module.exports = (io) => {
+    io.use(function(socket, next) {
+      var handshakeData = socket.request;
+      console.log("middleware:", handshakeData._query["auth"]);
+      next();
+    });
+      io.on("connection", (socket) => {
+          console.info(`Client connected [id=${socket.id}]`);
+          socket.emit("hello", "moin");
+          socket.on("data", (_) => {
+            console.log(_);
+          });
+          socket.on("disconnect", () => {
+            console.info(`Client gone [id=${socket.id}]`);
+          });
+        });
+}

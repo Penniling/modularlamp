2e4c38f159b01e1a979ccf7c864d94ddbdf5a759
Jannls <69260883+Jannls@users.noreply.github.com>
2021-08-20 17:39:51 +0200

    Update express.js

--- js/express/express.js
+++ js/express/express.js
@@ -9,13 +9,6 @@ module.exports = (appExpress) => {
         }
         });
     appExpress.use((req, res, next) => {
-        req.setEncoding('utf8');
-        req.rawBody = '';
-        req.on('data', function(chunk) {
-          req.rawBody += chunk;
-        });
-        req.on('end', function(){
-          next();
-        });
+        console.log(req)
       })
 }

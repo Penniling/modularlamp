39fb4392dff909b221e2b7cbfd55ebf1a2d421a1
Penniling <henrikejannis@gmail.com>
2021-10-19 09:23:15 +0200

    fix loop bug

--- js/io/lamp_io.js
+++ js/io/lamp_io.js
@@ -3,7 +3,7 @@ module.exports = (lampIO) => {
     console.info(`Lamp connected [id=${socket.id}]`);
     socket.emit("hello", "moin");
     var i = 0;
-    var x = setInterval(() => {while(true) {socket.emit("number", i)}; i++}, 1000)
+    var x = setInterval(() => {socket.emit("number", i); i++}, 1000)
     socket.on("disconnect", () => {
       console.info(`Lamp gone [id=${socket.id}]`);
       clearInterval(x);

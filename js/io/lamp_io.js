fecb927ddf4b1015bd77ebb5b55dcb04cc78a83a
Penniling <henrikejannis@gmail.com>
2021-10-19 19:55:46 +0200

    temporarily data function

--- js/io/lamp_io.js
+++ js/io/lamp_io.js
@@ -1,9 +1,12 @@
 module.exports = (lampIO) => {
-  lampIO.on("connection", (socket) => {
+  lamIOp.on("connection", (socket) => {
     console.info(`Lamp connected [id=${socket.id}]`);
     socket.emit("hello", "moin");
     var i = 0;
     var x = setInterval(() => {socket.emit("number", i); i++}, 1000)
+    socket.on("data", (_) => {
+      console.log(_);
+    })
     socket.on("disconnect", () => {
       console.info(`Lamp gone [id=${socket.id}]`);
       clearInterval(x);

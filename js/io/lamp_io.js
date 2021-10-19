879abfd3468613a1d0e2b5d071f14052e3d84616
Penniling <henrikejannis@gmail.com>
2021-10-19 09:20:45 +0200

    interval emitter to lamps

--- js/io/lamp_io.js
+++ js/io/lamp_io.js
@@ -1,21 +1,12 @@
 module.exports = (lampIO) => {
-
-  function lampUpdater(socket) {
-    var n = 0
-    while(true) {
-      setTimeout(() => {
-        socket.emit("Hello " + n)
-        n++
-      }, 5000)
-    }
-  }
-
   lampIO.on("connection", (socket) => {
     console.info(`Lamp connected [id=${socket.id}]`);
     socket.emit("hello", "moin");
-    lampUpdater(socket)
+    var i = 0;
+    var x = setInterval(() => {while(true) {socket.emit("number", i)}; i++}, 1000)
     socket.on("disconnect", () => {
       console.info(`Lamp gone [id=${socket.id}]`);
+      clearInterval(x);
     });
   });
 }

8ae8f7ba7faaa23d62fa13e1e99bb5986f38f4ce
Penniling <henrikejannis@gmail.com>
2021-11-13 20:05:39 +0100

    removed interval emitter in lamp io

--- backend/src/js/io/lamp_io.js
+++ backend/src/js/io/lamp_io.js
@@ -2,8 +2,6 @@ module.exports = (lampIO) => {
   lampIO.on("connection", (socket) => {
     console.info(`Lamp connected [id=${socket.id}]`);
     socket.emit("hello", "moin");
-    var i = 0;
-    var x = setInterval(() => {socket.emit("number", i); i++}, 1000)
     socket.on("data", (_) => {
       console.log(_);
     })

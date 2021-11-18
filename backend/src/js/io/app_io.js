35c41626d1964257d2ff890feee4b58ced758843
Penniling <henrikejannis@gmail.com>
2021-11-18 18:13:35 +0100

    added check token

--- backend/src/js/io/app_io.js
+++ backend/src/js/io/app_io.js
@@ -1,6 +1,8 @@
 module.exports = (appIO, lampIO, con) => {
   appIO.use((socket, next) => {
+    var token = socket.request._query.token
     console.log(socket.request._query.token)
+    console.log(checkAuthToken(token, con))
     next()
   })
 }

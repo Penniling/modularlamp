ae466d50d8ac17cceca665a8b88e7321f9cdfce5
Penniling <henrikejannis@gmail.com>
2021-11-18 17:18:57 +0100

    test token compare

--- backend/src/js/io/app_io.js
+++ backend/src/js/io/app_io.js
@@ -1,5 +1,17 @@
-module.exports = (appIO, lampIO) => {
-  appIO.use(function(socket, next) {
-    next();
-  })
+module.exports = (appIO, lampIO, con) => {
+  io.use(function(socket, next) {
+    var token = socket.request.query.token;
+    console.log(token)
+    if (checkAuthToken(token)) {
+      next();
+    }
+  });
+}
+
+function checkAuthToken(token) {
+  con.query('SELECT NAME FROM Users WHERE tokens="?"', [token], (error, results, fields) => {
+    if (error) return false;
+    console.log(results)
+    return true;
+})
 }

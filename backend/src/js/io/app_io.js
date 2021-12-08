7a26c960998335353d631df8ac841fae0116a84a
Penniling <henrikejannis@gmail.com>
2021-12-08 07:27:47 +0100

    started basic auth

--- backend/src/js/io/app_io.js
+++ backend/src/js/io/app_io.js
@@ -2,15 +2,6 @@ module.exports = (appIO, lampIO, con) => {
   appIO.use((socket, next) => {
     var token = socket.request._query.token
     console.log(socket.request._query.token)
-    console.log(checkAuthToken(token, con))
     next()
   })
-}
-
-function checkAuthToken(token, con) {
-  con.query("SELECT `name` FROM `Users` WHERE tokens=?", [token], (error, results, fields) => {
-    if (error) return false;
-    console.log(results)
-    return true;
-})
 }

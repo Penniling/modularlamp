410edaacbb253440736bd3f8c8a0972bcdec6b7d
Penniling <henrikejannis@gmail.com>
2021-12-13 18:44:54 +0100

    parse

--- backend/src/js/io/app_io.js
+++ backend/src/js/io/app_io.js
@@ -1,13 +1,13 @@
 module.exports = (appIO, lampIO, con) => {
   appIO.use((socket, next) => {
-    var creds = socket.request._query.creds
+    var creds = JSON.parse(socket.request._query.creds)
     console.log(creds)
     validateCreds(creds)
     next()
   })
 
   function validateCreds(creds) {
-    con.query('SELECT name FROM Users WHERE NAME="?" AND PASSWORD="?', [creds.usr, creds.pwd], (error, results, fields) => {
+    con.query('SELECT name FROM Users WHERE NAME=? AND PASSWORD=?', [creds.usr, creds.pwd], (error, results, fields) => {
       if (error) console.log(error)
       console.log(results)
     })

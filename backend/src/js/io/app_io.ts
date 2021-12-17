140b8e9dee107e780ab75b1d0ba8ad3e4f773b66
Penniling <henrikejannis@gmail.com>
2021-12-17 20:24:56 +0100

    123456789

--- backend/src/js/io/app_io.ts
+++ backend/src/js/io/app_io.ts
@@ -1,22 +1,25 @@
-const bcrypt = require("bcrypt")
+import bcrypt from "bcrypt"
 
-module.exports = (appIO, lampIO, con) => {
-  appIO.use((socket, next) => {
+export default (appIO, lampIO, con) => {
+  appIO.use(async (socket, next) => {
     var creds = JSON.parse(socket.request._query.creds)
-    console.log(creds)
-    if (validateCreds(creds))
+    
+    if(await validateCreds(creds)) {
+
+    }
     next()
   })
 
   async function validateCreds(creds) {
     return new Promise((resolve, reject) => {
-      con.query("SELECT * FROM Users WHERE NAME=? AND PASSWORD=?", [creds.usr, creds.pwd], (error, result, fields) => {
+      con.query("SELECT PASSWORD FROM Users WHERE NAME=?", [creds.usr], (error, result, fields) => {
         if(error || result?.length === 0) {
           console.log(error)
           reject(error)
         }
-        
-        if(result[0]?.name === creds.usr) {
+        var oldHash = bcrypt.hash(result[0].password, 10)
+        console.log(oldHash)
+        if(result[0]?.password === creds.usr) {
           resolve(creds.usr)
         }else {
           reject()
